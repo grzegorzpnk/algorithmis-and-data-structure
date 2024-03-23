4e00b55fca16ea72255fdd6f829244758500e172
Grzegorz Panek <grzegorzpnk@gmail.com>
2024-03-23 03:07:36 +0100

    new version

--- arrays/main.go
+++ arrays/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"learning/hackerRank/ArraysAndHash"
 )
 
@@ -19,8 +20,9 @@ func main() {
 	//slice.PrintSlice()
 	//
 
-	nums := []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}
-	ArraysAndHash.LongestConsecutive(nums)
+	//nums := []int{0, 3, 7, 2, 5, 8, 4, 1x6, 0, 1}
+	nums2 := []int{0, -1}
+	fmt.Println(ArraysAndHash.LongestConsecutive(nums2))
 
 	//fmt.Println(ArraysAndHash.TopKFrequent([]int{5, 5, 5, 5, 1, 1, 1, 2, 2, 3, 8, 8, 8, 8, 8, 8, 8}, 2))
 	//fmt.Println(ArraysAndHash.TopKFrequent([]int{1, 2}, 2))
--- hackerRank/ArraysAndHash/longestConsecutive.go
+++ hackerRank/ArraysAndHash/longestConsecutive.go
@@ -26,6 +26,45 @@ package ArraysAndHash
 //	//return maxSequenceLen
 //}
 
+//func max(a, b int) int {
+//	if a > b {
+//		return a
+//	}
+//	return b
+//}
+
+func LongestConsecutive(nums []int) int {
+
+	var result = 0
+	consecMap := make(map[int]bool)
+
+	for _, v := range nums {
+		consecMap[v] = true
+	}
+
+	cnt := 1
+	for _, v := range nums {
+		if consecMap[v-1] {
+			continue
+		} else {
+			for i := 1; i < len(nums); i++ {
+				if consecMap[v+i] {
+					cnt += 1
+				} else {
+					result = max(result, cnt)
+					cnt = 0
+					break
+				}
+			}
+		}
+	}
+
+	result = max(result, cnt)
+
+	return result
+
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
