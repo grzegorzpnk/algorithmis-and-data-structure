a1b405da4092791a67abf33cc611784d535503e1
Grzegorz Panek <grzegorzpnk@gmail.com>
2025-02-27 00:50:26 +0100

    new leetcode

--- leetCode/ArraysAndHash/topKFrequeqntWords.go
+++ leetCode/ArraysAndHash/topKFrequeqntWords.go
@@ -6,18 +6,21 @@ func topKFrequentWords(words []string, k int) []string {
 
 	freqMap := make(map[string]int)
 
+	//O(n)
 	for _, v := range words {
 		freqMap[v]++
 	}
 
 	bucket := make([][]string, len(words)+1)
 
+	//O(n)
 	for word, occurency := range freqMap {
 		bucket[occurency] = append(bucket[occurency], word)
 	}
 
 	ans := make([]string, 0, k)
 
+	//O(n)
 	for i := len(bucket) - 1; i >= 0; i-- {
 		if len(bucket[i]) > 1 {
 			sort.Strings(bucket[i])
@@ -33,4 +36,5 @@ func topKFrequentWords(words []string, k int) []string {
 
 	return ans
 
+	//)
 }
